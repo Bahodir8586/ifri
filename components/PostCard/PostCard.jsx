d9e29c73ecbb8a79241e1f77e6883373b164d558
Abdullayev Bahodir <bahodira213@gmail.com>
2021-12-06 16:33:53 +0500

    code improvement

--- components/PostCard/PostCard.jsx
+++ components/PostCard/PostCard.jsx
@@ -1,4 +1,44 @@
-const PostCard = () => {
-  return <></>
+import Link from 'next/link'
+
+const PostCard = ({ post }) => {
+  return (
+    <Link href={`/posts/${post.category.id}/post/${post.id}`} passHref={true}>
+      <div className="flex flex-col cursor-pointer transform hover:scale-105 transition duration-300 rounded-lg shadow-lg overflow-hidden">
+        <div className="flex-shrink-0">
+          <img
+            className="h-48 w-full object-cover"
+            src={post.imageUrl}
+            alt=""
+          />
+        </div>
+        <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+          <div className="flex-1">
+            <p className="text-sm font-medium text-indigo-600">
+              <Link href={`/posts/${post.category.id}/1`} passHref={true}>
+                <a className="hover:underline">{post.category.name}</a>
+              </Link>
+            </p>
+            <a href={post.href} className="block mt-2">
+              <p className="text-xl font-semibold text-gray-900">
+                {post.title}
+              </p>
+              <p className="mt-3 text-base text-gray-500">{post.description}</p>
+            </a>
+          </div>
+          <div className="mt-6 flex items-center">
+            <div className="flex-shrink-0">
+              <span className="sr-only">{post.author}</span>
+            </div>
+            <div className="ml-3">
+              <p className="text-sm font-medium text-gray-900">{post.author}</p>
+              <div className="flex space-x-1 text-sm text-gray-500">
+                <time dateTime={post.datetime}>{post.date}</time>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
 }
 export default PostCard
--- pages/posts/[category]/[page]/index.jsx
+++ pages/posts/[category]/[page]/index.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import PostCard from '../../../../components/PostCard'
 
 import Layout from '../../../../layout'
 
@@ -39,6 +40,18 @@ const posts = [
       'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
     author: 'Daniela Metz',
   },
+  {
+    id: 4,
+    title: 'Boost your conversion rate',
+    category: { name: 'Science', id: '1' },
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    imageUrl:
+      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    author: 'Roel Aufderehar',
+  },
 ]
 
 export async function getStaticPaths() {
@@ -71,58 +84,9 @@ const Page = ({ categoryName }) => {
               libero labore natus atque, ducimus sed.
             </p>
           </div>
-          <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+          <div className="mt-12 mx-auto grid gap-5 md:grid-cols-2 lg:grid-cols-3 lg:max-w-none">
             {posts.map((post) => (
-              <Link
-                key={post.title}
-                href={`/posts/${post.category.id}/post/${post.id}`}
-                passHref={true}
-              >
-                <div className="flex flex-col cursor-pointer transform hover:scale-105 transition duration-200 rounded-lg shadow-lg overflow-hidden">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-48 w-full object-cover"
-                      src={post.imageUrl}
-                      alt=""
-                    />
-                  </div>
-                  <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                    <div className="flex-1">
-                      <p className="text-sm font-medium text-indigo-600">
-                        <Link
-                          href={`/posts/${post.category.id}/1`}
-                          passHref={true}
-                        >
-                          <a className="hover:underline">
-                            {post.category.name}
-                          </a>
-                        </Link>
-                      </p>
-                      <a href={post.href} className="block mt-2">
-                        <p className="text-xl font-semibold text-gray-900">
-                          {post.title}
-                        </p>
-                        <p className="mt-3 text-base text-gray-500">
-                          {post.description}
-                        </p>
-                      </a>
-                    </div>
-                    <div className="mt-6 flex items-center">
-                      <div className="flex-shrink-0">
-                        <span className="sr-only">{post.author}</span>
-                      </div>
-                      <div className="ml-3">
-                        <p className="text-sm font-medium text-gray-900">
-                          {post.author}
-                        </p>
-                        <div className="flex space-x-1 text-sm text-gray-500">
-                          <time dateTime={post.datetime}>{post.date}</time>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </Link>
+              <PostCard post={post} key={post.id} />
             ))}
           </div>
         </div>
