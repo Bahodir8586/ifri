4d05d3561924a83459179bb1cafc88667733cad7
Abdullayev Bahodir <bahodira213@gmail.com>
2022-01-17 15:29:24 +0500

    update

--- pages/posts/[category]/[page]/index.jsx
+++ pages/posts/[category]/[page]/index.jsx
@@ -107,10 +107,7 @@ const Page = ({ categoryName }) => {
                 }`}
                 passHref={true}
               >
-                <a
-                  href="#"
-                  className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-                >
+                <a className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
                   Next
                 </a>
               </Link>
@@ -122,4 +119,4 @@ const Page = ({ categoryName }) => {
   )
 }
 
-export default Page;
+export default Page
